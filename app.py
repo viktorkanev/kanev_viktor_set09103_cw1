041b7b59adf08a92ac8eb610172f6ff725047ea7
viktorkanev <viktor.kanev9@gmail.com>
2018-10-16 16:08:14 +0100

    added filter function to filter artists

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, jsonify, request, session, url_for, redirect
+from artists import Artists
+import bcrypt
+
 
 app = Flask(__name__)
+
+Artists = Artists()
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/collections')
+def collections():
+   return render_template('collections.html', artists=Artists)
+
+
+@app.route('/collections/<string:name>/')
+def artist(name):
+    return render_template('artists.html', name=name, artists=Artists)
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
---
+++ artists.py
@@ -0,0 +1,34 @@
+def Artists():
+    artists = [
+        {
+            'id': 1,
+            'name': 'Eminem',
+            'genre': 'Hip Hop',
+            'albums': [{
+                'title': 'LP',
+                'release_date': '28-02-2018',
+                'songs': '28'
+            },
+                {
+                    'title': 'Recovery',
+                    'release_date': '28-02-2018',
+                    'songs': '12'
+            }]
+        },
+        {
+            'id': 2,
+            'name': '50 cent',
+            'genre': 'Hip Hop'
+        },
+        {
+            'id': 3,
+            'name': 'Eagles',
+            'genre': 'Rock'
+        },
+        {
+            'id': 4,
+            'name': 'Kings Of Leon',
+            'genre': 'Rock'
+        }
+    ]
+    return artists
