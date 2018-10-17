c5bea7cc2dc5dd980ca45d938b3d7fb1beeb261a
viktorkanev <viktor.kanev9@gmail.com>
2018-10-17 16:18:07 +0100

    implemented login functionality

--- app.py
+++ app.py
@@ -1,12 +1,49 @@
-from flask import Flask, render_template, json, jsonify, request, session, url_for, redirect
+from flask import Flask, render_template, json, request, session, url_for, redirect, flash
 from artists import Artists
+from users import Users
 import bcrypt
+from functools import wraps
 
 
 app = Flask(__name__)
-
+app.secret_key = 'A0Zr98j /3 yX R~XHH!jmN]LWX/,?RT'
 Artists = Artists()
+Users = Users()
 
+def check_auth(email, password):
+   for user in Users:
+        useremail = user.get('email')
+        userpw = user.get('password')
+        print(userpw)
+   if(email == useremail and  password == userpw):
+        return True
+   return False
+
+def requires_login(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        status = session.get('logged_in', False)
+        if not status:
+            return redirect(url_for('.root'))
+        return f(*args, **kwargs)
+    return decorated
+
+@app.route('/logout/')
+def logout():
+    session['logged_in'] = False
+    return redirect(url_for('.root'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def root():
+    if request.method == 'POST':
+        user = request.form['email']
+        pw = request.form['password']
+
+        if check_auth(request.form['email'],request.form['password']):
+            session['logged_in']= True
+            flash('You were successfully logged in!')
+            return redirect(url_for('.collections'))
+    return render_template('login.html')
 
 @app.route('/')
 def home():
--- users.py
+++ users.py
@@ -1,9 +1,17 @@
 def Users():
     users = [
         {
-            'id':1,
-            'email':'admin@admin.com',
-            'password':'admin',
-            'isAdmin':'yes'
+            'id': 1,
+            'email': 'admin@admin.com',
+            'password': 'admin',
+            'isAdmin': 'yes'
+        },
+        {
+            'id': 1,
+            'email': 'tito@admin.com',
+            'password': 'admin',
+            'isAdmin': 'yes'
         }
     ]
+
+    return users
